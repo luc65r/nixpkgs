f87bb4b1e03e77d2d130f7815561daa84cc794f7
luc65r <lucas@ransan.tk>
2020-05-09 17:32:02 +0200

    bitwarden-rofi: init at a53cc1e21097c201a56b560b8f634a032330d61b

---
+++ pkgs/tools/security/bitwarden-rofi/default.nix
@@ -0,0 +1,55 @@
+{ stdenvNoCC
+, fetchFromGitHub
+, rofi
+, bitwarden-cli
+, jq
+, keyutils
+
+, x11Support ? !stdenvNoCC.isDarwin, xclip, xsel, xdotool
+, waylandSupport ? false, wl-clipboard, ydotool
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "bitwarden-rofi";
+  version = "a53cc1e21097c201a56b560b8f634a032330d61b";
+
+  src = fetchFromGitHub {
+    owner = "mattydebie";
+    repo = "bitwarden-rofi";
+    rev = version;
+    sha256 = "0cwpc3am9kqn9pxqq8kaqg8150y3bln8a6gzm5nfh61357m55xba";
+  };
+
+  buildInputs = [
+    rofi
+    bitwarden-cli
+    jq
+    keyutils
+  ] ++ stdenvNoCC.lib.optionals x11Support [
+    xclip
+    xsel
+    xdotool
+  ] ++ stdenvNoCC.lib.optionals waylandSupport [
+    wl-clipboard
+    ydotool
+  ];
+
+  dontBuild = true;
+
+  postPatch = ''
+    sed -i "s|^DIR=.\+$|DIR='$out/lib'|" bwmenu
+  '';
+
+  installPhase = ''
+    install -Dm755 -t $out/bin bwmenu
+    install -Dm644 -t $out/lib lib-bwmenu
+  '';
+
+  meta = with stdenvNoCC.lib; {
+    description = "Wrapper for Bitwarden and Rofi";
+    homepage = "https://github.com/mattydebie/bitwarden-rofi";
+    license = licenses.gpl3;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ luc65r albakham ];
+  };
+}
